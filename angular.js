3b6b1811b67acc1274936baf02d33414875f08dc
zhouxg2 <zhouxg2@lenovo.com>
2016-12-14 17:32:58 +0800

    lesson 9

--- angular.js
+++ angular.js
@@ -3,24 +3,40 @@
  */
 var app = angular.module('scopeExample', [])
 
-app.factory('Data',function(){
-   return {message:"test"}
-});
+//lesson 1--------------------------
+//app.factory('Data',function(){
+//   return {message:"test"}
+//});
+//
+//app.filter('reverse',function(Data){
+//    return function (text){
+//        return text.split("").reverse().join("") + Data.message;
+//    }
+//});
+//
+//app.controller('test1', ['$scope','Data', function($scope,Data) {
+//        $scope.data = Data;
+//    }]);
+//
+//app.controller('test2', ['$scope','Data', function($scope,Data) {
+//    $scope.data = Data;
+//
+//    $scope.reverseMess = function(msg){
+//        return msg.split("").reverse().join("")
+//    }
+//}]);
 
-app.filter('reverse',function(Data){
-    return function (text){
-        return text.split("").reverse().join("") + Data.message;
-    }
-});
+//lesson 8-------------------------------------------------
+//
+//app.factory('Avenger',function(){
+//    var Avenger = {};
+//    Avenger.cast=[{'name':1},{'name':2},{'name':3},{'name':4}];
+//    return Avenger;
+//});
+//
+//app.controller('avgctl',['$scope','Avenger',function($scope,Avenger){
+//    $scope.aaa = Avenger;
+//}]);
 
-app.controller('test1', ['$scope','Data', function($scope,Data) {
-        $scope.data = Data;
-    }]);
+//lesson9-------------------------------------
 
-app.controller('test2', ['$scope','Data', function($scope,Data) {
-    $scope.data = Data;
-
-    $scope.reverseMess = function(msg){
-        return msg.split("").reverse().join("")
-    }
-}]);
